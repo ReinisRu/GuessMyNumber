cc59f04359ed12f487a4416129ac5891225b86dd
ReinisRu <rr@itfly.lv>
2021-01-03 22:56:28 +0200

    working code and style added bofore adjustments

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GuessMyNumber
+
 Simple JavaScript game. Practice challenge from Udemy course by Jonas Schmedtmann https://www.udemy.com/share/101WeYCUEYc1ZSRXg=/
---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+document.querySelector(`.check`).addEventListener(`click`, function () {
+  const guess = Number(document.querySelector(`.guess`).value);
+  console.log(guess, typeof guess);
+
+  //   when there is no input
+  if (!guess) {
+    displayMessage(`⛔️ No number!`);
+
+    // when player wins
+  } else if (guess === secretNumber) {
+    displayMessage("Correct Number!🎉");
+    document.querySelector(`.number`).textContent = secretNumber;
+    document.querySelector(`body`).style.backgroundColor = `#60b347`;
+    document.querySelector(`.number`).style.width = `30rem`;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(`.highscore`).textContent = highscore;
+    }
+
+    // when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+// Reset the game!
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector(`.number`).textContent = `?`;
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(`.guess`).value = ``;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.number`).style.width = `15rem`;
+});
